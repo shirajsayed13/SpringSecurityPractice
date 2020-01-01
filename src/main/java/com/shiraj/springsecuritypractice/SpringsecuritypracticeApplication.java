a414d984bf5e2a95f02cdc5eceac310d90d7e05f
Shiraj Sayed <shirajsayed13@gmail.com>
2020-01-01 11:04:19 +0530

    basic auth in application class

--- src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
+++ src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
@@ -2,12 +2,25 @@ package com.shiraj.springsecuritypractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class SpringsecuritypracticeApplication {
+@EnableWebSecurity
+public class SpringsecuritypracticeApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringsecuritypracticeApplication.class, args);
 	}
 
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.anyRequest()
+				.authenticated()
+				.and()
+				.httpBasic();
+	}
 }
