bba3964df1ecc0d792918426a6c556e632743765
Shiraj Sayed <shirajsayed13@gmail.com>
2020-01-01 11:01:02 +0530

    initial setup

---
+++ src/test/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiraj.springsecuritypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritypracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
