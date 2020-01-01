290a94451ed00af197eafad764f79fc5c2a2aff2
Shiraj Sayed <shirajsayed13@gmail.com>
2020-01-01 21:33:23 +0530

    add JwtConfig and secret key setup

--- src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
+++ src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
@@ -1,24 +1,24 @@
 package com.shiraj.springsecuritypractice;
 
+import com.shiraj.springsecuritypractice.auth.ApplicationUserService;
+import com.shiraj.springsecuritypractice.jwt.JwtConfig;
+import com.shiraj.springsecuritypractice.jwt.JwtTokenVerifier;
+import com.shiraj.springsecuritypractice.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import java.util.concurrent.TimeUnit;
+import javax.crypto.SecretKey;
 
-import static com.shiraj.springsecuritypractice.ApplicationUserPermission.COURSE_WRITE;
 import static com.shiraj.springsecuritypractice.ApplicationUserRole.*;
 
 @SpringBootApplication
@@ -32,45 +32,37 @@ public class SpringsecuritypracticeApplication extends WebSecurityConfigurerAdap
 
     private final PasswordEncoder passwordEncoder;
 
+    private final ApplicationUserService applicationUserService;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
     @Autowired
-    public SpringsecuritypracticeApplication(PasswordEncoder passwordEncoder) {
+    public SpringsecuritypracticeApplication(PasswordEncoder passwordEncoder,
+                                             ApplicationUserService applicationUserService, SecretKey secretKey, JwtConfig jwtConfig) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //.csrf().disable()
-                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager(), jwtConfig, secretKey))
+                .addFilterAfter(new JwtTokenVerifier(secretKey, jwtConfig), JwtUsernameAndPasswordAuthenticationFilter.class)
+                //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                /*.antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())*/
                 .anyRequest()
-                .authenticated()
-                .and()
-				.httpBasic();
-
-				// cookie code
-                //.and().rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)); // custom time of 21 days
-                //.and().rememberMe(); // Default time 2-weeks
-
-				// logout code
-				/*.and()
-                .logout()
-                .logoutUrl("/logout")
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID", "remember-me")
-                .logoutSuccessUrl("/login");*/
+                .authenticated();
     }
 
 
-    @Override
+   /* @Override
     @Bean
     protected UserDetailsService userDetailsService() {
         UserDetails userShiraj = User.builder()
@@ -100,6 +92,51 @@ public class SpringsecuritypracticeApplication extends WebSecurityConfigurerAdap
                 userJames,
                 userTom
         );
+    }*/
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
     }
 
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+    }
+
+    public void formBasedLogin() {
+        /*.antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())*/
+
+        //.httpBasic();
+
+        /* // formLogin
+
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/courses", true)
+                .passwordParameter("password")
+                .usernameParameter("username")
+
+                // cookie code
+
+                .and().rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21)); // custom time of 21 days
+                .and().rememberMe(); // Default time 2-weeks
+
+                // logout code
+
+				.and()
+                .logout()
+                .logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID", "remember-me")
+                .logoutSuccessUrl("/login");*/
+    }
 }
---
+++ src/main/java/com/shiraj/springsecuritypractice/auth/ApplicationUserDao.java
@@ -0,0 +1,8 @@
+package com.shiraj.springsecuritypractice.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+
+    public Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+}
