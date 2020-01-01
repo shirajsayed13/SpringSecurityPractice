95937576eee63e5b01fd3fa25ebcb4b75f9334e2
Shiraj Sayed <shirajsayed13@gmail.com>
2020-01-01 11:19:51 +0530

    Use InMemoryUserDataBase

--- src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
+++ src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
@@ -2,9 +2,14 @@ package com.shiraj.springsecuritypractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @SpringBootApplication
 @EnableWebSecurity
@@ -25,4 +30,20 @@ public class SpringsecuritypracticeApplication extends WebSecurityConfigurerAdap
 				.and()
 				.httpBasic();
 	}
+
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails userDetails = User.builder()
+				.username("shiraj")
+				.password("password")
+				.roles("STUDENT")
+				.build();
+
+		return new InMemoryUserDetailsManager(
+				userDetails
+		);
+	}
+
 }
