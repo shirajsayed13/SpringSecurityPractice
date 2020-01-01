d29f7f907c4e6d0fba67d90ebc261eaf892403f4
Shiraj Sayed <shirajsayed13@gmail.com>
2020-01-01 14:10:08 +0530

    Setup Authorities and permission

--- src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
+++ src/main/java/com/shiraj/springsecuritypractice/SpringsecuritypracticeApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,11 +15,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.shiraj.springsecuritypractice.ApplicationUserRole.ADMIN;
-import static com.shiraj.springsecuritypractice.ApplicationUserRole.STUDENT;
+import static com.shiraj.springsecuritypractice.ApplicationUserPermission.COURSE_WRITE;
+import static com.shiraj.springsecuritypractice.ApplicationUserRole.*;
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringsecuritypracticeApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
@@ -33,9 +36,15 @@ public class SpringsecuritypracticeApplication extends WebSecurityConfigurerAdap
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
+		http
+				.csrf().disable()
+				.authorizeRequests()
 				.antMatchers("/","index","/css/*","/js/*").permitAll()
 				.antMatchers("/api/**").hasRole(STUDENT.name())
+				/*.antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+				.antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+				.antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+				.antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())*/
 				.anyRequest()
 				.authenticated()
 				.and()
@@ -46,20 +55,32 @@ public class SpringsecuritypracticeApplication extends WebSecurityConfigurerAdap
 	@Override
 	@Bean
 	protected UserDetailsService userDetailsService() {
-		UserDetails usersShiraj = User.builder()
+		UserDetails userShiraj = User.builder()
 				.username("shiraj")
 				.password(passwordEncoder.encode("password"))
-				.roles(STUDENT.name())
+//				.roles(STUDENT.name()) //ROLE_STUDENT
+				.authorities(STUDENT.getSimpleGrantedAuthorities())
 				.build();
 
-		UserDetails usersJames = User.builder()
-				.username("james")
-				.password(passwordEncoder.encode("pass123"))
-				.roles(ADMIN.name())
+		UserDetails userJames = User.builder()
+				.username("linda")
+				.password(passwordEncoder.encode("password123"))
+//				.roles(ADMIN.name())  //ROLE_ADMIN
+				.authorities(ADMIN.getSimpleGrantedAuthorities())
 				.build();
+
+		UserDetails userTom = User.builder()
+				.username("tom")
+				.password(passwordEncoder.encode("password123"))
+//				.roles(ADMINTRAINEE.name()) //ROLE_ADMINTRAINEE
+				.authorities(ADMINTRAINEE.getSimpleGrantedAuthorities())
+				.build();
+
 
 		return new InMemoryUserDetailsManager(
-				usersShiraj, usersJames
+				userShiraj,
+				userJames,
+				userTom
 		);
 	}
 
